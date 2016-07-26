2e6f61c03973d7b6a231d2a8eba48568d48fc8d8
Matthias Kaiser <m4ttkaiser@googlemail.com>
2016-07-26 17:11:11 +0200

    Adding a Javassist/Weld/JBossInterceptor gadgets to ysoserial.

--- src/main/java/ysoserial/payloads/JBossInterceptors.java
+++ src/main/java/ysoserial/payloads/JBossInterceptors.java
@@ -25,7 +25,7 @@ import java.util.*;
     by @matthias_kaiser
 */
 @SuppressWarnings({"rawtypes", "unchecked"})
-@Dependencies({"javassist:javassist:3.12.1.GA", "org.jboss.interceptor:jboss-interceptor-core:2.0.0.Final" })
+@Dependencies({"javassist:javassist:3.12.1.GA", "org.jboss.interceptor:jboss-interceptor-core:2.0.0.Final", "javax.enterprise:cdi-api:1.0-SP1", "javax.interceptor:javax.interceptor-api:3.1", "org.jboss.interceptor:jboss-interceptor-spi:2.0.0.Final", "org.slf4j:slf4j-api:1.7.21"})
 public class JBossInterceptors implements ObjectPayload<Object> {
 
     public Object getObject(final String command) throws Exception {
--- src/main/java/ysoserial/payloads/JavassistWeld1.java
+++ src/main/java/ysoserial/payloads/JavassistWeld1.java
@@ -25,7 +25,7 @@ import java.util.*;
     by @matthias_kaiser
 */
 @SuppressWarnings({"rawtypes", "unchecked"})
-@Dependencies({"javassist:javassist:3.12.1.GA", "org.jboss.weld:weld-core:1.1.33.Final"})
+@Dependencies({"javassist:javassist:3.12.1.GA", "org.jboss.weld:weld-core:1.1.33.Final", "javax.enterprise:cdi-api:1.0-SP1", "javax.interceptor:javax.interceptor-api:3.1","org.jboss.interceptor:jboss-interceptor-spi:2.0.0.Final", "org.slf4j:slf4j-api:1.7.21"})
 public class JavassistWeld1 implements ObjectPayload<Object> {
 
     public Object getObject(final String command) throws Exception {
