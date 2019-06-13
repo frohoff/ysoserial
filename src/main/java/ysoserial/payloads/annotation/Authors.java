f7fad382b853653f96eacae32f305f00c4deffe9
sciccone <sciccone@gdssecurity.com>
2019-06-13 12:18:40 +0100

    added Atomikos gadget payload

--- src/main/java/ysoserial/payloads/annotation/Authors.java
+++ src/main/java/ysoserial/payloads/annotation/Authors.java
@@ -23,6 +23,7 @@ public @interface Authors {
     String SCRISTALLI = "scristalli";
     String HANYRAX = "hanyrax";
     String EDOARDOVIGNATI = "EdoardoVignati";
+    String SCICCONE = "stefano_ciccone";
 
     String[] value() default {};
 
