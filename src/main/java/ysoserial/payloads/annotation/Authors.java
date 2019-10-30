fe839ad2bfe2bc606e2532b71c6c4a3612acb8a0
Yorick Koster <ykoster@users.noreply.github.com>
2019-10-30 21:42:33 +0100

    Add Jython2 gadget
    This version of Jython2 executes a command through os.system(). Based on Jython1 from @pwntester & @cschneider4711

--- src/main/java/ysoserial/payloads/annotation/Authors.java
+++ src/main/java/ysoserial/payloads/annotation/Authors.java
@@ -23,6 +23,7 @@ public @interface Authors {
     String SCRISTALLI = "scristalli";
     String HANYRAX = "hanyrax";
     String EDOARDOVIGNATI = "EdoardoVignati";
+    String YKOSTER = "ykoster";
 
     String[] value() default {};
 
