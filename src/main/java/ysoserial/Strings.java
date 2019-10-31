3369310e9cddd88c260a7ec6dbd30f2236ae5e22
Kristian <kristianint@outlook.com>
2019-10-31 17:18:37 +0100

    Fix formatTable to support smaller terminals, fix DynamicDependencies display

--- src/main/java/ysoserial/Strings.java
+++ src/main/java/ysoserial/Strings.java
@@ -1,6 +1,6 @@
 package ysoserial;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -29,9 +29,25 @@ public class Strings {
 
     public static List<String> formatTable(List<String[]> rows) {
         final Integer[] maxLengths = new Integer[rows.get(0).length];
-        for (String[] row : rows) {
+        // Max column width, will try to split on spaces if possible
+        // Uses 210 as default terminal width, if not specified as system property
+        final int maxColumnWidth = Integer.getInteger("terminalWidth", 140) / maxLengths.length;
+        for(int index = 0; index < rows.size(); index++) {
+            String[] row = rows.get(index);
             if (maxLengths.length != row.length) throw new IllegalStateException("mismatched columns");
             for (int i = 0; i < maxLengths.length; i++) {
+                if (row[i].length() > maxColumnWidth) {
+                    row[i] = WordUtils.wrap(row[i], maxColumnWidth);
+                    String[] split = row[i].split("\n");
+                    rows.get(index)[i] = split[0];
+                    for (int ii = 1; ii < split.length; ii++) {
+                        if (rows.size() <= index + ii || rows.get(index + ii)[i].length() > 0) {
+                            rows.add(index + ii, new String[maxLengths.length]);
+                            Arrays.fill(rows.get(index + ii), "");
+                        }
+                        rows.get(index + ii)[i] = split[ii];
+                    }
+                }
                 if (maxLengths[i] == null || maxLengths[i] < row[i].length()) {
                     maxLengths[i] = row[i].length();
                 }
@@ -40,7 +56,8 @@ public class Strings {
 
         final List<String> lines = new LinkedList<String>();
         for (String[] row : rows) {
-            for (int i = 0; i < maxLengths.length; i++) {
+            // No need to fill last line with spaces
+            for (int i = 0; i < maxLengths.length - 1; i++) {
                 final String pad = repeat(" ", maxLengths[i] - row[i].length());
                 row[i] = row[i] + pad;
             }
