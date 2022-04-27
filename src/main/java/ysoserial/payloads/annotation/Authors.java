9c448b58cd4533b0d4c3bb00229c29e293be11af
navalorenzo <34750199+navalorenzo@users.noreply.github.com>
2022-04-27 07:04:18 +0200

    CommonsCollections8 payload (#116)

--- src/main/java/ysoserial/payloads/annotation/Authors.java
+++ src/main/java/ysoserial/payloads/annotation/Authors.java
@@ -23,6 +23,7 @@ public @interface Authors {
     String SCRISTALLI = "scristalli";
     String HANYRAX = "hanyrax";
     String EDOARDOVIGNATI = "EdoardoVignati";
+    String NAVALORENZO = "navalorenzo";
     String JANG = "Jang";
     String ARTSPLOIT = "artsploit";
 
